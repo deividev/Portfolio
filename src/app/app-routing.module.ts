f8483b47ad51c8b021cfd52863281e0a93fa024f
David seco <deivid.develop@gmail.com>
2023-04-08 00:22:35 +0200

    Feat: create route projects

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ProjectsComponent } from './pages/projects/projects.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'projects',
+    component: ProjectsComponent,
+    data: {state: 'projects'}
+  },
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
