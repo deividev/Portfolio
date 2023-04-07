74036452ee5695dae8e4c753e3caa05e608eb7f4
David seco <deivid.develop@gmail.com>
2023-04-08 00:18:56 +0200

    Feat: create toggleButton theme component

---
+++ src/app/shared/ui/toggle-button/toggle-button.component.ts
@@ -0,0 +1,18 @@
+import { Component, Inject, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-toggle-button',
+  templateUrl: './toggle-button.component.html',
+  styleUrls: ['./toggle-button.component.scss']
+})
+export class ToggleButtonComponent implements OnInit {
+  ngOnInit(): void {  
+    document.querySelector('body')?.setAttribute('data-theme', 'dark');
+  }
+
+  toggleMode(event: any): void {
+    if (event.target.checked) document.querySelector('body')?.setAttribute('data-theme', 'light');
+    else document.querySelector('body')?.setAttribute('data-theme', 'dark');
+  }
+
+}
